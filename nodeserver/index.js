e2b2f7807aaf33637fe30dccd2cd1fd4a2bf57e0
vishwajitprasad06 <151417252+vishwajitprasad06@users.noreply.github.com>
2026-05-21 21:04:06 +0530

    Update index.js

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -1,22 +1,36 @@
-const { Server } = require("socket.io");
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 
-const io = new Server(8000, {
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
     cors: {
         origin: "*"
     }
+});
+
+const PORT = process.env.PORT || 8000;
+
+// Home Route
+app.get('/', (req, res) => {
+    res.send("Chat server is running");
 });
 
 const users = {};
 
 io.on("connection", socket => {
 
-   console.log("User connected");
+    console.log("User connected");
 
     socket.on("new-user-joined", name => {
 
         users[socket.id] = name;
 
         socket.broadcast.emit("user-joined", name);
+
     });
 
     socket.on("send", message => {
@@ -33,8 +47,13 @@ io.on("connection", socket => {
         socket.broadcast.emit("left", users[socket.id]);
 
         delete users[socket.id];
+
     });
 
 });
 
-console.log("Server running on port 8000");
+server.listen(PORT, () => {
+
+    console.log(`Server running on port ${PORT}`);
+
+});
